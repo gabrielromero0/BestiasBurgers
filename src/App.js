9373f84b8bb82b411f08cdbe05e6b5c766893c2f
Nicolás Muros <nicomuros@gmail.com>
2023-09-19 15:32:56 -0300

    feat: cart context

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import ProductList from './Components/ProductList/ProductList';
 import './styles/globals.css';
 import PageNotFound from './Components/PageNotFound/PageNotFound';
 import ProductDetail from './Components/ProductDetail/ProductDetail';
+import CustomNavbar from './Components/NavbarContainer/NavbarContainer';
+import CartContextProvider from './context/CartContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/producto/:idProducto" element={<ProductDetail />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <CartContextProvider> {/* GESTIÓN DEL CARRITO*/}
+      <BrowserRouter>
+        <CustomNavbar />
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/producto/:idProducto" element={<ProductDetail />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+      </CartContextProvider>
   );
 }
 
